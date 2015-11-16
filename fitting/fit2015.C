409476ca756492f3a1b3fbc2c35405f906b152d1
Emilien Chapon <emilien.chapon@cern.ch>
2015-11-16 17:08:41 +0100

    First working version

--- fitting/fit2015.C
+++ fitting/fit2015.C
@@ -1,9 +1,27 @@
-#include "makeWorkspace.C"
+#include "makeWorkspace2015.C"
+#include "buildModelUpsi2015.C"
 
 void fit2015() {
    RooWorkspace myws;
-   makeWorkspace(myws, 1, -2.4, 2.4, 0, 100, 0, 2.4, 0, 50, 0, 200);
+   makeWorkspace2015(myws, 1, -2.4, 2.4, 0, 0, 0, 2.4, 0, 50, 0, 200);
 
    buildModelUpsi2015(myws, 2, 3);
 
+   RooRealVar* mass =(RooRealVar*) myws.var("invariantMass"); //
+   RooDataSet* data0_fit =(RooDataSet*) myws.data("data");
+   RooAbsPdf* pdf = (RooAbsPdf*) myws.pdf("pdf");
+   RooFitResult* fitObject = pdf->fitTo(*data0_fit,Save(),Hesse(kTRUE),Extended(kTRUE)); // Fit
+   pdf->Print();
+   Double_t baseNll = fitObject->minNll();
+   // RooMinuit m(*nll);
+   RooRealVar *nsig1f   =(RooRealVar*) myws.var("N_{ #varUpsilon(1S)}");
+
+   RooArgSet allvars = myws.allVars();
+   int npars= allvars.getSize() ;
+   int nbins = ceil((mass_h-mass_l)/binw); // all of which are 'globally' defined in the .h header
+   RooPlot* frame = mass->frame(Bins(nbins),Range(mass_l,mass_h));  
+   data0_fit->plotOn(frame);// data drawn first for pdf object to pick the proper normalisation
+   pdf->plotOn(frame,Name("thePdf"));
+
+   frame->Draw();
 }
